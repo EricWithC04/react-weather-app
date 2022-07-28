4ade1952f7fc022dbf865d4b0fb4e3f866352666
EricWithC04 <negativegame@gmail.com>
2022-07-27 23:07:53 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+    return (
+        <>
+            <h1>Componente de React</h1>
+        </>
+    )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+
+const devMode = process.env.NODE_ENV !== "production";
+
+module.exports = {
+    entry: [
+        "./src/index.js"
+    ],
+    output: {
+        path: __dirname + "/build",
+        filename: "bundler.js"
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+               test: /\.js$/,
+               loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    devMode ? "style-loader": MiniCssExtractPlugin.loader,
+                    "css-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new MiniCSSExtractPlugin({
+            filename: "bundler.css"
+        })
+    ]
+}
